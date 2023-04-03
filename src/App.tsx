fb73a3a61dd132b9a0e3d731ec88d4e9f3b30958
Naral <77269201+NaralC@users.noreply.github.com>
2023-04-03 22:00:40 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import { usePagination } from "@mantine/hooks";
+
+function App() {
+  const mockResults = [
+    "Sensei Senseless",
+    "Jamal",
+    "Ben Dover",
+    "Jack Menough",
+    "Timothy Bichboy",
+    "Glenn Treafuker",
+    "Chad Thundercock",
+    "Rajesh Hsejar",
+    "Caleb Cantroast",
+    "Chase Wood",
+    "Ray Cist",
+    "Jack Hoffmen",
+    "E. Norma Scock",
+    "Vye Brator",
+    "Noah Dultin",
+    "Emily Wokerson",
+    "Kent Taekehent",
+    "Justin Case",
+  
+  ];
+
+  return <div className="text-black h-screen text-center px-14 py-3">yo</div>;
+}
+
+export default App;
