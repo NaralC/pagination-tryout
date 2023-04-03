4cd7c83ae8d3d111ae0479cd2d34f425afbf61fc
Naral <77269201+NaralC@users.noreply.github.com>
2023-04-03 22:33:24 +0100

    done
    yay
    - wait it's this easy

--- src/App.tsx
+++ src/App.tsx
@@ -31,16 +31,31 @@ function App() {
   const pagination = usePagination({
     total: Math.ceil(mockResults.length / ITEMS_PER_PAGE),
     initialPage: 1,
-    onChange(page) {
-      
+    onChange: (page) => {
+      const start = (page - 1) * ITEMS_PER_PAGE;
+      const end = start + ITEMS_PER_PAGE;
+
+      setVisibleResults(mockResults.slice(start, end));
     },
   });
 
   return (
-    <div className="h-screen text-center flex flex-col justify-center align-middle gap-5">
-      {visibleResults.map((person) => {
-        return <div>{person}</div>;
-      })}
+    <div className="h-screen text-center flex flex-col justify-center align-middle gap-5 text-3xl">
+      <div>
+        {visibleResults.map((result, idx) => {
+          return (
+            <div key={idx} className="my-5">
+              {result}
+            </div>
+          );
+        })}
+      </div>
+      <div>——————————</div>
+      <div className="flex justify-center gap-5">
+        <button onClick={pagination.previous}>&lt;</button>
+        <div>Current page: {pagination.active}</div>
+        <button onClick={pagination.next}>&gt;</button>
+      </div>
     </div>
   );
 }
