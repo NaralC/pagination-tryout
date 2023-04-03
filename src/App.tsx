e85459feba15b83ddf2308f3d8868a07f77cd358
Naral <77269201+NaralC@users.noreply.github.com>
2023-04-03 22:23:22 +0100

    setup mantine hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,48 @@
 import { useState } from "react";
 import { usePagination } from "@mantine/hooks";
 
-function App() {
-  const mockResults = [
-    "Sensei Senseless",
-    "Jamal",
-    "Ben Dover",
-    "Jack Menough",
-    "Timothy Bichboy",
-    "Glenn Treafuker",
-    "Chad Thundercock",
-    "Rajesh Hsejar",
-    "Caleb Cantroast",
-    "Chase Wood",
-    "Ray Cist",
-    "Jack Hoffmen",
-    "E. Norma Scock",
-    "Vye Brator",
-    "Noah Dultin",
-    "Emily Wokerson",
-    "Kent Taekehent",
-    "Justin Case",
-  
-  ];
+const ITEMS_PER_PAGE = 5;
+const mockResults = [
+  "Sensei Senseless",
+  "Jamal",
+  "Ben Dover",
+  "Jack Menough",
+  "Timothy Bichboy",
+  "Glenn Treafuker",
+  "Chad Thundercock",
+  "Rajesh Hsejar",
+  "Caleb Cantroast",
+  "Chase Wood",
+  "Ray Cist",
+  "Jack Hoffmen",
+  "E. Norma Scock",
+  "Vye Brator",
+  "Noah Dultin",
+  "Emily Wokerson",
+  "Kent Taekehent",
+  "Justin Case",
+];
 
-  return <div className="text-black h-screen text-center px-14 py-3">yo</div>;
+function App() {
+  const [visibleResults, setVisibleResults] = useState<string[]>(
+    mockResults.slice(0, ITEMS_PER_PAGE)
+  );
+
+  const pagination = usePagination({
+    total: Math.ceil(mockResults.length / ITEMS_PER_PAGE),
+    initialPage: 1,
+    onChange(page) {
+      
+    },
+  });
+
+  return (
+    <div className="h-screen text-center flex flex-col justify-center align-middle gap-5">
+      {visibleResults.map((person) => {
+        return <div>{person}</div>;
+      })}
+    </div>
+  );
 }
 
 export default App;
